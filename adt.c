0800980e14d3e3d2d7be2bcb1d0c61f46609e23a
rushilamla <155717418+rushilamla@users.noreply.github.com>
2024-03-04 16:34:09 +0530

    list.c

--- adt.c
+++ adt.c
@@ -1,90 +1,92 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define maxsize 1000
-int n;
-void create(int list[]){
-    int i;
-    printf("enter the size of list : ");
-    scanf("%d",&n);
-    if(n>maxsize){
-        printf("list over flow!!!");
-    }
-    else{
-        printf("enter list elements : \n");
-        for(i=0;i<n;i++){
-            scanf("%d",&list[i]);
-        }
-    }
+int list[maxsize],size;
+void Create()
+{
+	int i;
+	printf("Enter the size of list : ");
+	scanf("%d",&size);
+	if(size>maxsize)
+		printf("List Over flow !!!!");
+	else{
+		printf("Enter List Elments : \n");
+		for(i=0; i<size; i++)
+			scanf("%d",&list[i]);
+	}
 }
-void display(int list[]){
-    if(n==0){
-        printf("list is empty");
-    }
-    else{
-        printf("list elements are : \n");
-    int i;
-    for(i=0;i<n;i++){
-        printf("%d\t",list[i]);
-    }
-    }
+void Display(){
+	if(size==0)
+		printf("List is Empty !!!!\n");
+	else{
+		printf("List Elements are : \n");
+		int i;
+		for(i=0; i<size; i++)
+			printf("%d\t",list[i]);
+		}
+		printf("\n");
 }
-void insert(int list[]){
-    int pos,value;
-    printf("enter position : ");
-    scanf("%d",&pos);
-    printf("enter value to insert : ");
-    scanf("%d",value);
-    if(pos<n){
-        for(int i=n; i>=pos;i--){
-            list[i]=list[i-1];
-            list[pos]=list[value];
-            n++;
-        }
-        printf("%d",list[pos]);
-    }
-    else{
-        printf("invalid position!!!\n");
-    }
+void Insert(){
+	if(size==0)
+		printf("List is Empty !!!\n");
+	else{
+		int pos, value,i;
+		printf("Enter Position Where You Want To Insert New Element : ");
+		scanf("%d",&pos);
+		printf("Enter Value To Insert : ");
+		scanf("%d",&value);
+		if(pos>0 && pos<=size){
+			for(i=size; i>=pos; i--)
+				list[i]=list[i-1];
+			list[pos-1]=value;
+			size++;
+			printf("Elemented Added !!!!!\n");
+		}
+		else
+			printf("Invalid Position !!!!\n\n");	
+	}
+}
+void delete(){
+	if(size==0)
+		printf("List is Empty !!!!\n");
+	else{
+		int pos,i;
+		printf("Enter Position of the element you want to delete: ");
+		scanf("%d",&pos);
+		if(pos>0 && pos<=size){
+			for(i=pos-1; i<size; i++)
+				list[i]=list[i+1];
+			size--;
+			printf("Element Deleted !!!!\n");
+		}
+		else
+			printf("Invalid Position !!!!\n");
+	}
+}
+void search(){
+	if(size==0)
+		printf("List id Empty !!!\n");
+	else{
+		int val,i;
+		printf("Enter Value To Search : ");
+		scanf("%d",&val);
+		for(i=0; i<size; i++){
+			if(list[i]==val){
+				break;
+			}
+		}
+		if(i==size)
+			printf("Element Not Found !!!!\n");
+		else
+			printf("Element Found !!!!\n");
+	}
 }
 int main(){
-    int list[maxsize];
-    while(1){ 
-        system("cls");
-        printf("1. Create List\n"); //storing data in list but data cannot be more than list size
-        printf("2. Insert Elements List\n");
-        printf("3. Display All List Elements\n");
-        printf("4. Delete element for a List\n");
-        printf("5. Search element in list\n");     
-        printf("6. Exit\n");   
-    
-    int choice;
-    printf("enter choice : ");
-    scanf("%d",&choice);
-
-    switch(choice){
-
-        case 1:
-            create(list);
-            break;
-        case 2:
-            insert(list);
-            break;
-        case 3:
-            display(list);
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
-        case 6:
-            exit(0);
-            break;
-        default :
-            printf("invalid choice!!\n");
-        
-        
-    }
-    system("pause");
-    }
-    return 0;
+	Create();
+	Insert();
+	Display();
+	delete();
+	Display();
+	search();
+	return 0;
 }
